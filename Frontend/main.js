cf490a88fd105c598f997cb06b7f6117e50edca7
Md Obaid <mdobaid0778@gmail.com>
2026-05-18 11:51:48 +0530

    .ece file build

--- Frontend/main.js
+++ Frontend/main.js
@@ -1,13 +1,21 @@
-import { app, BrowserWindow
-} from "electron";
+import { app, BrowserWindow } from "electron";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
     height: 800,
-    });
+  });
 
-  win.loadURL("http://localhost:5173");
+  const indexPath = path.join(__dirname, "dist", "index.html");
+
+  win.loadFile(indexPath);
+
+  win.webContents.openDevTools();
 }
 
 app.whenReady().then(() => {
